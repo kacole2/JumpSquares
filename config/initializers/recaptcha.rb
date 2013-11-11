946490872fd159bb96ca4bedc7fb36c6ad7bf27d
kacole2 <kendrickcoleman@gmail.com>
2013-11-11 18:34:45 -0500

    Delete recaptcha.rb
    i dont want this public

--- config/initializers/recaptcha.rb
+++
@@ -1,8 +0,0 @@
-#./config/initializes/recaptcha.rb
-# from github.com/ambethia/recaptcha README.rdoc
-Recaptcha.configure do |config|
-
-    config.public_key  = ENV['PUBLICKEY']
-    config.private_key = ENV['PRIVATEKEY']
-
-  end
