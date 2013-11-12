d9864f8a1b11eb0c7216a69d3e15056b61c15a6d
Kendrick Coleman <kendrickcoleman@gmail.com>
2013-11-12 12:56:08 -0500

    CarrierWave to S3 configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWSACCESS'],                        # required
     :aws_secret_access_key  => ENV['AWSSECRET'],                        # required
-    :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
+    #:region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
     #:host                   => 's3.example.com',             # optional, defaults to nil
-    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    #:endpoint               => 'https://jumpsquares.s3-website-us-east-1.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = 'jumpsquares'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_public     = false                              # optional, defaults to true
+  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
