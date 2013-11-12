6e378501f9f52cd5099da224eb0a62d3c8df1c94
Kendrick Coleman <kendrickcoleman@gmail.com>
2013-11-11 23:54:21 -0500

    fixed captcha load

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,20 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
 def create
-      if verify_recaptcha
-        super
-      else
-        build_resource
-        clean_up_passwords(resource)
-        flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."      
-        flash.delete :recaptcha_error
-        render :new
-      end
+    if !verify_recaptcha
+      flash.delete :recaptcha_error
+      build_resource
+      resource.valid?
+      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render :new }
+    else
+      flash.delete :recaptcha_error
+      super
     end
+  end
+  
+  def clean_up_passwords(*args)
+    # Delete or comment out this method to prevent the password fields from 
+    # repopulating after a failed registration
+  end
 end
