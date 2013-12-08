b68f9e6b4940b8e7278ff8be3091ef2ab296622d
Kendrick Coleman <kendrickcoleman@gmail.com>
2013-12-07 23:48:44 -0500

    rdp8

--- app/controllers/jumpsquares_controller.rb
+++ app/controllers/jumpsquares_controller.rb
@@ -94,7 +94,6 @@ class JumpsquaresController < ApplicationController
   def createrdpfile
     jumpsquare = Jumpsquare.find(params[:id])
     file = Tempfile.new(jumpsquare.ipordns.to_s + '.rdp')
-    file.path = "#{Rails.root}/tmp"
     file.write("screen mode id:i:2
 desktopwidth:i:1436
 desktopheight:i:925
