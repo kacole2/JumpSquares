d2e1f1d6a8d7c45d0ef2289ff05e39dcde8f1bca
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-01-08 11:02:15 -0500

    added nmap scans to admin dashboard

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -21,6 +21,14 @@ class AdminDashboardController < ApplicationController
       @jumpsquare_rdp_count = Jumpsquare.count(:all, :conditions => { :remotetype => 'rdp' })
       @jumpsquare_ssh_count = Jumpsquare.count(:all, :conditions => { :remotetype => 'ssh' })
       @jumpsquare_vnc_count = Jumpsquare.count(:all, :conditions => { :remotetype => 'vnc' })
+      @nmapxml_count = Nmapfile.count(:all)
+      @nmapfiles = Nmapfile.all
+      @nmaptotalrecord_count = 0
+      @nmapmissingrecord_count = 0
+      @nmapfiles.each do |nmapfile|
+        @nmaptotalrecord_count = nmapfile.nmapfilerecords.to_i + @nmaptotalrecord_count
+        @nmapmissingrecord_count = nmapfile.nmapfilematches.to_i + @nmapmissingrecord_count
+      end
     end
 
 end
