086616e8016796296bf13d885874ff423ff83b46
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-06-05 00:04:06 -0400

    try with blank

--- app/controllers/jumpsquares_controller.rb
+++ app/controllers/jumpsquares_controller.rb
@@ -10,7 +10,7 @@ class JumpsquaresController < ApplicationController
    @tags = Tag.where(:tagcreator => current_user.email)
    @jumpsizes = Jumpsize.where(:jumpsizecreator => current_user.email)
    
-   if @jumpsizes.first.sortorder == nil
+   if @jumpsizes.first.sortorder.blank?
      @jumpsizes.first.sortorder == 'name'
    end
    
