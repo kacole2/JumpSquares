d133c5a2b29270f306cc00b10cdbf4a51f96501d
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-06-05 00:59:44 -0400

    added CDN and changed sort order

--- app/controllers/jumpsquares_controller.rb
+++ app/controllers/jumpsquares_controller.rb
@@ -12,16 +12,14 @@ class JumpsquaresController < ApplicationController
    
    if @jumpsizes.first.sortorder.nil?
      @jumpsizes.first.sortorder == 'name'
-   else 
-     @jumpsizes.first.sortorder == 'name'
    end
    
    if current_user.has_role? :admin
       #uncomment to see ALL jumpsquares
       #@jumpsquares = Jumpsquare.paginate(:page => params[:page], :per_page => @jumpsizes.first.itemsperpage).search(params[:search]).find(:all, :order => sort_order('name'))
-      @jumpsquares = Jumpsquare.search(params[:search]).paginate(:page => params[:page], :per_page => @jumpsizes.first.itemsperpage).where(:jscreator => current_user.email).order(sort_order('name'))
+      @jumpsquares = Jumpsquare.search(params[:search]).paginate(:page => params[:page], :per_page => @jumpsizes.first.itemsperpage).where(:jscreator => current_user.email).order(sort_order(@jumpsizes.first.sortorder))
     else
-      @jumpsquares = Jumpsquare.search(params[:search]).paginate(:page => params[:page], :per_page => @jumpsizes.first.itemsperpage).where(:jscreator => current_user.email).order(sort_order('name'))
+      @jumpsquares = Jumpsquare.search(params[:search]).paginate(:page => params[:page], :per_page => @jumpsizes.first.itemsperpage).where(:jscreator => current_user.email).order(sort_order(@jumpsizes.first.sortorder))
     end 
   end
   
